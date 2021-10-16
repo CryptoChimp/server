2a8cecd65f539b409e8547c41dc15da44882daaa
MaximilianHagelstam <maximilian.hagelstam@gmail.com>
2021-10-16 11:07:11 +0300

    Remove cross-env

--- src/server.js
+++ src/server.js
@@ -3,8 +3,6 @@ const app = require('./app');
 const { PORT } = process.env || 8080;
 
 app.listen(PORT, () => {
-  console.log(
-    `Server running in ${process.env.NODE_ENV} on http://localhost:${PORT}`,
-  );
+  console.log(`Server running on http://localhost:${PORT}`);
   console.log('Press CTRL-C to stop\n');
 });
